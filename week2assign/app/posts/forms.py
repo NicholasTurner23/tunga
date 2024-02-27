a3947ed22b6902ddd1aa997d0e264f547a5e6914
NicholasTurner23 <mr.nnturner@gmail.com>
2024-02-27 14:40:28 +0300

    Update post forms

--- week2assign/app/posts/forms.py
+++ week2assign/app/posts/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, IntegerField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length
 
 class PostFrom(FlaskForm):
-    title = StringField("Title", validators=[DataRequired(), Length(32)])
-    description = TextAreaField("Description", validators=[DataRequired(), Length(255)])
-    author = StringField("Author", validators=[DataRequired(), Length(32)])
+    title = StringField("Title", validators=[DataRequired(), Length(10, 32)])
+    description = TextAreaField("Description", validators=[DataRequired(), Length(20, 255)])
+    # author = SelectField('User', coerce=int, validators=[DataRequired()], choices=[(user.id, user.username) for user in User.query.all()])
     submit = SubmitField("Submit")
