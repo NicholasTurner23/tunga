3878b89bbc7d793d21855659c64d4f314e747686
NicholasTurner23 <mr.nnturner@gmail.com>
2024-02-27 14:41:53 +0300

    Require login, and use user as author

--- week2assign/app/posts/models.py
+++ week2assign/app/posts/models.py
@@ -1,15 +1,22 @@
 from app import get_db
+from sqlalchemy.sql.expression import text
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.orm import relationship
+from app.users.models import User
+from datetime import datetime
 
 db = get_db()
 
 class BlogPost(db.Model):
     __tablename__ = "posts"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(32), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    id = Column(Integer, primary_key=True)
+    title = Column(String(128), nullable=False)
+    body = Column(Text, nullable=False)
+    author_id = Column(Integer, ForeignKey("users.id", ondelete='CASCADE'), nullable=False)
+    author = relationship(User, backref='posts')
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return f"BlogPost('{self.title}','{self.author}','{self.created_at}')"
--- week2assign/app/posts/routes.py
+++ week2assign/app/posts/routes.py
@@ -1,21 +1,26 @@
 from app.posts import postbp
 from typing import Dict
-from flask import request, render_template, redirect, Response, flash
+from flask import render_template, redirect, Response, flash
+from flask_login import login_required, current_user
 import os, json
 from datetime import datetime
 from .forms import PostFrom
 from .models import BlogPost
 from app import get_db
+from app.users.models import User
 
 db = get_db()
 @postbp.route('/posts/create', methods=["GET", "POST"])
+@login_required
 def createPost():
     form = PostFrom()
-    if form.validate_on_submit():
+    user_id = current_user.get_id()
+    user = User.query.filter_by(id=user_id).first()
+    if form.validate_on_submit() and current_user.is_authenticated:
         new_post = BlogPost(
             title = form.title.data,
             body = form.description.data,
-            author = form.author.data,
+            author = user,
             created_at = datetime.now(),
             updated_at = datetime.now()
         )
@@ -36,7 +41,8 @@ def getPosts() -> str:
     posts = BlogPost.query.all()
     return render_template("posts.html", data=posts)
 
-@postbp.route('/posts/delete/<int:post_id>', methods=["POST", "DELETE"])
+@postbp.route('/posts/delete/<int:post_id>', methods=["GET", "DELETE"])
+@login_required
 def deletePost(post_id) -> Response:
     BlogPost.query.filter_by(id=post_id).delete()
     db.session.commit()
