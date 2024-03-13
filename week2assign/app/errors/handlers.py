a1cf5be33e7d0870381f56b1fc4674479e25891b
NicholasTurner23 <mr.nnturner@gmail.com>
2024-03-13 18:03:41 +0300

    Add custom error handler

---
+++ week2assign/app/errors/handlers.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app.errors import errors
+
+
+@errors.app_errorhandler(403)
+def error_403(e):
+    return render_template("/errors/error.html", error_num=403, error_text='Please check your account and try again.'), 403
+
+@errors.app_errorhandler(404)
+def error_404(e):
+    return render_template("/errors/error.html", error_num=404, error_text=' The page you’re looking for doesn’t exist.'), 404
+
+@errors.app_errorhandler(500)
+def error_500(e):
+    return render_template("/errors/error.html", error_num=500, error_text="We're experiencing some trouble on our end. "
+                                                                   "Please try again in the near future."), 500
