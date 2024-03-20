356be2927bdb480874849f2c780dee936b2f0801
NicholasTurner23 <mr.nnturner@gmail.com>
2024-03-20 03:58:19 +0300

    Add blog users

--- django_intro/blog_user/views.py
+++ django_intro/blog_user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django_tables2.views import SingleTableMixin
+from django_filters.views import FilterView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from . import filters, tables, models
+from blog_post.models import Posts
 
-# Create your views here.
+
+class UserList(LoginRequiredMixin, SingleTableMixin, FilterView, ListView):
+    model = models.User
+    table_class = tables.UserTable
+    filterset_class = filters.UserFilter
+    paginate_by = 50
+
+
+class DetailUser(DetailView):
+    model = models.User
+    success_url = reverse_lazy("blog_user:users")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        author_id = self.kwargs["pk"]
+        author = self.request.user
+        if author_id != author.id:
+            author = models.User.objects.filter(id=author_id).first()
+        
+        if author:
+            posts = Posts.objects.filter(author=author)
+        context['posts'] = posts
+        return context
+
